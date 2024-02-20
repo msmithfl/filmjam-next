79ac518a9ff5df6db8fe3c661a5039ca7ca4e44a
msmithfl <msmithfl@icloud.com>
2024-02-20 00:31:41 -0500

    fixed clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,20 +1,26 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import { Button } from "../ui/button";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+// import NavItems from "./NavItems";
+// import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <h1 className="text-2xl font-bold">filmjam.io</h1>
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
         </Link>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
-            {/* <MobileNav /> */}
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
