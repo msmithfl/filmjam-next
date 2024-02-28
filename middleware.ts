fb6a8f8ce3c5284c19022ce7dbfe61f2583f73e9
msmithfl <msmithfl@icloud.com>
2024-02-28 15:52:54 -0500

    middleware.ts fix for clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/watch", "/jams", "/community", "api/webhook/clerk"],
+  publicRoutes: ["/", "/watch", "/jams", "/community", "/api/webhook/clerk"],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ["api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
